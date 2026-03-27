fb6107badf6401d1f35b726c5650c7d43008baf4
Senku <senku@Senku-01.Senku>
2026-03-27 21:50:02 +0700

    fix: restore missing closing div in dashboard

--- src/components/grademaster/DashboardLayer.tsx
+++ src/components/grademaster/DashboardLayer.tsx
@@ -368,6 +368,7 @@ export default function DashboardLayer({
                 </button>
               </>
             )}
+          </div>
         </div>
 
         {/* Remedial Announcement Banner */}
